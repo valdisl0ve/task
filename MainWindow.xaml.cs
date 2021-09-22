29beac5a0b7e53e239ae30c6a0f0d93014490568
valdisl0ve <75386722+valdisl0ve@users.noreply.github.com>
2021-09-22 19:22:30 +0300

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+//using System.Data.SQLite;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -14,77 +15,151 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace tusks
+namespace ToDo
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        AppContext db;
 
-        Dictionary<string, string> list_of_tusk = new Dictionary<string, string>();
+        
+
+        Dictionary<string, string> taskList = new Dictionary<string, string>();
         public MainWindow()
         {
             InitializeComponent();
-            //List<string> test = new List<string>() { "To buy", "To do", " To read", "Else" };
+            db = new AppContext();
 
-            
-            list_of_tusk.Add("To buy", "to buy.txt");
-            list_of_tusk.Add("To do", "to do.txt");
-            list_of_tusk.Add("To read", "to read.txt");
-            list_of_tusk.Add("Else", "else.txt");
+            //db.DB_connect = new SQLiteConnection("Data Source=db.db");
+            //db.DB_connect.Open();
+
+            create.Visibility = Visibility.Collapsed;
+            backButton.Visibility = Visibility.Hidden;
+            todoContentAll.Visibility = Visibility.Collapsed;
 
 
-            /*
-           foreach (object item in test)
-           {
-               tusklist.Items.Add((string)item);
-           }
-            */
 
-            foreach (object item in list_of_tusk.Keys)
+            taskList.Add("To buy", "to buy.txt");
+            // taskList.Add("To do", "to do.txt");
+            // taskList.Add("To read", "to read.txt");
+            // taskList.Add("Else", "else.txt");
+
+
+            foreach (object item in taskList.Keys)
             {
-                tusklist.Items.Add((string)item);
+                listboxTask.Items.Add((string)item);
             }
-        }
+
+
+         
+    
+
+    }
 
         private void add_Click(object sender, RoutedEventArgs e)
         {
-            list_of_tusk.Add(new_tusk.Text, new_tusk + ".txt");
+           
+
+        }
+
+        private void new_Click(object sender, RoutedEventArgs e)
+        {
+            start.Visibility = Visibility.Collapsed;
+            create.Visibility = Visibility.Visible;
+            backButton.Visibility = Visibility.Visible;
+        }
+
+        private void ok_Click(object sender, RoutedEventArgs e)
+        {
+            start.Visibility = Visibility.Visible;
+            create.Visibility = Visibility.Collapsed;
+
+            taskList.Add(newTask.Text, newTask.Text + ".txt");
 
 
-            using (FileStream fs = File.Create(AppDomain.CurrentDomain.BaseDirectory + new_tusk.Text + ".txt"))
+            using (FileStream fs = File.Create(AppDomain.CurrentDomain.BaseDirectory + newTask.Text + ".txt"))
             {
                 byte[] info = new UTF8Encoding(true).GetBytes("Write something to do later");
                 fs.Write(info, 0, info.Length);
             }
 
 
-            tusklist.Items.Clear();
-            foreach (object item in list_of_tusk.Keys)
+            listboxTask.Items.Clear();
+            foreach (object item in taskList.Keys)
             {
-                tusklist.Items.Add((string)item);
+                listboxTask.Items.Add((string)item);
             }
+
+
+            Todo_table todo_table = new Todo_table(newTask.Text, "Write something to do late", 0);
+
+            db.Todo_table.Add(todo_table);
+            db.SaveChanges();
+
+
         }
 
-        private void show_Click(object sender, RoutedEventArgs e)
+        private void listboxTask_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            string choose = tusklist.SelectedItem.ToString();
+            newTask.Text = "";
+            todoContentAll.Visibility = Visibility.Visible;
+            backButton.Visibility = Visibility.Visible;
+            listboxTask.Visibility = Visibility.Collapsed;
+            Create_new_list.Visibility = Visibility.Hidden;
+            string choose = listboxTask.SelectedItem.ToString();
+            
 
-            //tusks.Content = 
+            
 
             using (StreamReader sr = new StreamReader(choose + ".txt"))
             {
-                tusks.Text = sr.ReadToEnd();
+                todoContent.Text = sr.ReadToEnd();
             }
         }
 
-        private void write_Click(object sender, RoutedEventArgs e)
+
+        private void backButton_Click(object sender, RoutedEventArgs e)
         {
-            using (FileStream fs = File.Create(AppDomain.CurrentDomain.BaseDirectory + tusklist.SelectedItem.ToString() + ".txt"))
+            start.Visibility = Visibility.Visible;
+            create.Visibility = Visibility.Collapsed;
+            backButton.Visibility = Visibility.Hidden;
+
+
+            todoContentAll.Visibility = Visibility.Collapsed;
+            listboxTask.Visibility = Visibility.Visible;
+            Create_new_list.Visibility = Visibility.Visible;
+
+
+
+
+        }
+
+        private void save_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            using (FileStream fs = File.Create(AppDomain.CurrentDomain.BaseDirectory + listboxTask.SelectedItem.ToString() + ".txt"))
             {
-                byte[] info = new UTF8Encoding(true).GetBytes(tusks.Text);
+                byte[] info = new UTF8Encoding(true).GetBytes(todoContent.Text);
                 fs.Write(info, 0, info.Length);
+            }
+
+
+
+        }
+
+        private void delete_Click(object sender, RoutedEventArgs e)
+        {
+          
+            taskList.Remove(listboxTask.SelectedItem.ToString());
+
+            listboxTask.Items.Clear();
+
+            foreach (object item in taskList.Keys)
+            {
+                listboxTask.Items.Add((string)item);
             }
         }
     }
